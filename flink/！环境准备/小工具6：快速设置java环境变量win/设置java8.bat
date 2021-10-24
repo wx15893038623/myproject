22ab79d121593fed58aa469ca3654516f751d482
Atticus <728986439@qq.com>
2021-10-24 21:55:16 +0800

    1024

---
+++ flink/！环境准备/小工具6：快速设置java环境变量win/设置java8.bat
@@ -0,0 +1,12 @@
+@echo off
+color 0a
+echo.------------------------------------
+echo.TODO:设置java环境变量
+echo.
+echo.------------------------------------
+::如果有的话，先删除JAVA_HOME
+wmic ENVIRONMENT where "name='JAVA_HOME'" delete
+
+::创建JAVA_HOME VariableValue 后面是JDK的安装路径,自行更改
+ wmic ENVIRONMENT create name="JAVA_HOME",username="<system>",VariableValue="F:\ProgramData\Java\jdk1.8.0_201"
+pause
